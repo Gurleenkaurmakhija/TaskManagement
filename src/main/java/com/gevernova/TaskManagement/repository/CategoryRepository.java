3365daec1dc749f2cd6401bc2d351930463c6c81
Gurleenkaurmakhija <makhijagurleen@gmail.com>
2026-02-09 08:14:14 +0530

    [GurleenKaur]: add Category Jpa repository for db communication

---
+++ src/main/java/com/gevernova/TaskManagement/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.gevernova.TaskManagement.repository;
+
+
+import com.gevernova.TaskManagement.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<User, Long> {
+}
+
